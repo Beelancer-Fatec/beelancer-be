23f24e612b745dc9e3449ac02df7692e7a910654
SouzaDiegoCl <souzadiegocl@gmail.com>
2025-05-09 17:18:21 -0300

    feat: Posts

--- Controllers/chamadosController.js
+++ Controllers/chamadosController.js
@@ -22,9 +22,51 @@ const createChamados = async (req, res) => {
       data_criacao,
       data_edicao,
       status,
+      status_servico,
       endereco,
     } = req.body;
-    await chamadosServices.cadChamados(client_id,
+    await chamadosServices.cadChamados(
+      client_id,
+      freelancer_id,
+      titulo,
+      descricao,
+      data_realizacao,
+      tipo_servico,
+      valor_a_pagar,
+      data_criacao,
+      data_edicao,
+      status,
+      status_servico,
+      endereco
+    );
+
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+const deleteChamados = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      await chamadosServices.delChamado(id);
+    } else {
+      res.sendStatus();
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+const updateChamado = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      const {
+        client_id,
         freelancer_id,
         titulo,
         descricao,
@@ -34,73 +76,46 @@ const createChamados = async (req, res) => {
         data_criacao,
         data_edicao,
         status,
-        endereco)
-        res.sendStatus(201)
+        endereco,
+      } = req.body;
+      await chamadosServices.updtChamado(id, {
+        client_id,
+        freelancer_id,
+        titulo,
+        descricao,
+        data_realizacao,
+        tipo_servico,
+        valor_a_pagar,
+        data_criacao,
+        data_edicao,
+        status,
+        endereco,
+      });
+    }
   } catch (error) {
     console.log(error);
-    res.sendStatus(500)
+    res.sendStatus(500);
   }
-}
+};
 
-const deleteChamados = async (req,res)=>{
-    try{
-    if(ObjectId.isValid(req.params.id)){
-        const id = req.params.id
-        await chamadosServices.delChamado(id)
-    }else{
-        res.sendStatus()
+const GetOneChamado = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      const chamado = await chamadosServices.getOne(id);
+      res.sendStatus(200).json({ chamado: chamado });
+    } else {
+      res.sendStatus(400);
     }
-  }catch(error){
-    console.log(error)
-    res.sendStatus(500)
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
   }
-}
-
-const updateChamado = async (req,res)=>{
-    try{
-        if(ObjectId.isValid(req.params.id)){
-            const id = req.params.id
-            const {client_id,
-                freelancer_id,
-                titulo,
-                descricao,
-                data_realizacao,
-                tipo_servico,
-                valor_a_pagar,
-                data_criacao,
-                data_edicao,
-                status,
-                endereco} = req.body
-                await chamadosServices.updtChamado(id,{client_id,
-                    freelancer_id,
-                    titulo,
-                    descricao,
-                    data_realizacao,
-                    tipo_servico,
-                    valor_a_pagar,
-                    data_criacao,
-                    data_edicao,
-                    status,
-                    endereco,})
-        }
-    }catch(error){
-        console.log(error)
-        res.sendStatus(500)
-    }
-}
-
-const GetOneChamado = async (req,res)=>{
-    try{
-        if(ObjectId.isValid(req.params.id)){
-            const id = req.params.id
-            const chamado = await chamadosServices.getOne(id)
-            res.sendStatus(200).json({chamado:chamado})
-        }else{
-            res.sendStatus(400)
-        }
-    }catch(error){
-        console.log(error)
-        res.sendStatus(500)
-    }
-}
-export default {createChamados,deleteChamados,updateChamado,getAllChamados,GetOneChamado}
+};
+export default {
+  createChamados,
+  deleteChamados,
+  updateChamado,
+  getAllChamados,
+  GetOneChamado,
+};
